319c089814717410cd520f6ec152c67ceb3acf8a
Alexis <omaroquendo221@gmail.com>
2020-10-21 16:42:23 -0500

    MVC

--- models/bicicleta.js
+++ models/bicicleta.js
@@ -12,8 +12,26 @@ Bicicleta.allBicis = [];
 Bicicleta.add = function(aBici){
     Bicicleta.allBicis.push(aBici); 
 }
+
+Bicicleta.findById = function(aBiciId){
+    var aBici = Bicicleta.allBicis.find(x => x.id==aBiciId);
+    if(aBici){
+        return aBici;
+    }else{
+        throw new Error('No existe Bicicleta con id ${aBiciId}');
+    }
+}
+    Bicicleta.removeById =function(aBiciId){
+        for(var i=0; i<Bicicleta.allBicis.length; i++){
+            if(Bicicleta.allBicis[i].id == aBiciId){
+                Bicicleta.allBicis.splice(i,1);
+                break;
+            }
+        }
+    }
     var a = new Bicicleta(1, 'rojo', 'urbana', [-34.6012424, -58.3861497]);
-    var b = new Bicicleta(2, "Amaraillo", 'urbana', [-34.5012424, -58.7861497])
+    var b = new Bicicleta(2, "Amaraillo", 'urbana', [-34.5012424, -58.7861497]);
     Bicicleta.add(a);
     Bicicleta.add(b);
+
 module.exports = Bicicleta;
