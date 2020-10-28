265416a1e62752a6e330a03c1c6d3c89ab9f39fb
Alexis <omaroquendo221@gmail.com>
2020-10-27 20:24:28 -0500

    Mongoose

--- models/bicicleta.js
+++ models/bicicleta.js
@@ -1,37 +1,30 @@
-var Bicicleta = function(id, modelo, color, ubicacion){
-    this.id = id;
-    this.modelo = modelo;
-    this.color = color;
-    this.ubicacion = ubicacion;
-}
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-Bicicleta.prototype.toString = function(){
-    return 'id: ' + this.id + " color: " + this.color;
-}
-Bicicleta.allBicis = [];
-Bicicleta.add = function(aBici){
-    Bicicleta.allBicis.push(aBici); 
-}
-
-Bicicleta.findById = function(aBiciId){
-    var aBici = Bicicleta.allBicis.find(x => x.id==aBiciId);
-    if(aBici){
-        return aBici;
-    }else{
-        throw new Error('No existe Bicicleta con id ${aBiciId}');
+var biciclteaSchema = new Schema({
+    code: Number,
+    color: String,
+    modelo: String,
+    ubicacion:{
+        type: [Number], index:{ type:'2dsphere', sparse: true}
     }
-}
-    Bicicleta.removeById =function(aBiciId){
-        for(var i=0; i<Bicicleta.allBicis.length; i++){
-            if(Bicicleta.allBicis[i].id == aBiciId){
-                Bicicleta.allBicis.splice(i,1);
-                break;
-            }
-        }
-    }
-    var a = new Bicicleta(1, 'rojo', 'urbana', [-34.6012424, -58.3861497]);
-    var b = new Bicicleta(2, "Amaraillo", 'urbana', [-34.5012424, -58.7861497]);
-    Bicicleta.add(a);
-    Bicicleta.add(b);
+});
 
-module.exports = Bicicleta;
+biciclteaSchema.statics.createInstance = function(code, color, modelo, ubicacion){
+    return new this({
+        code: code,
+        color: color,
+        modelo: modelo,
+        ubicacion: ubicacion
+    });
+};
+
+biciclteaSchema.methods.toString = function(){
+    return 'code: ' +this.code+ ' color: ' + this.color;
+};
+
+biciclteaSchema.statics.allBicis = function(cb){
+    return this.find({}, cb);
+}
+module.exports = mongoose.model('Bicicleta', biciclteaSchema);
+
